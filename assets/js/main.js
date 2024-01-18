edeb69011f39b353448b941ebef21fb237f81581
adibmansuri511 <adibmansuri.developer@gmail.com>
2024-01-18 22:59:48 +0530

    Update all files.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,61 @@
-// program to reverse the string
-function removeSpaces() {
+// program to check if a number is a float or integer value
 
-    // Take input from the user
-    const str = (document.getElementById("textInput").value.toString());
-    console.log(`Input text is : ${str}`);
+// Get button element and add click event listener on it and assign to element variable.
+const element = document.getElementById("myBtn");
 
-
-    // The split(' ') method is used to split the strings into individual array elements.
-    // The join('') method merges the array into a string.
-
-    const newStr = str.split(' ').join("");
-    console.log(newStr);
+element.addEventListener("click", checkNumber);
 
 
-    // const newStr = wordsArr
-    // console.log(newStr);
+function checkNumber(e) {
+    e.preventDefault();
+
+    // Take input value from the user and convert it into a number.
+    let inputValue = document.getElementById("inputValue").value;
+
+    console.log(`Input value is : ${inputValue}`);
+    console.log(`Input value data type is : ${typeof inputValue}`);
+
+    // check if the passed value is a number
+    // The typeof operator is used to check the data type of the passed value.
+    // The isNaN() method checks if the passed value is a number.
+
+    // Check the input field is not a blank.
+    if (inputValue) {
+
+        inputValue = Number(inputValue);
+        console.log(`Input value data type is : ${typeof inputValue}`);
 
 
-    document.querySelector('#h4').innerHTML = `Result : ${newStr}`;
+        if (typeof inputValue === 'number' && !isNaN(inputValue)) {
+
+            // check if it is integer
+            // The Number.isInteger() method is used to check if the number is an integer value.
+            if (Number.isInteger(inputValue)) {
+
+                document.querySelector('#h5').innerHTML = `Result : ${inputValue} is integer value.`;
+
+                console.log(`${inputValue} is integer value.`);
+
+            } else {
+
+                document.querySelector('#h5').innerHTML = `Result : ${inputValue} is a float value.`;
+
+                console.log(`${inputValue} is a float value.`);
+
+            }
+
+        } else {
+
+            document.querySelector('#h5').innerHTML = `Result : ${inputValue} is not a number.`;
+            console.log(`${inputValue} is not a number.`);
+
+        }
+
+    } else {
+
+        document.querySelector('#h5').innerHTML = `Please enter the value.`;
+        console.log(`Please enter the value.`);
+
+    }
+
 }
